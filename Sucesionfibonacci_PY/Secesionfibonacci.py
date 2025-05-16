ace24d99f9afdbca876982b6edc5097f6487bfec
juanfercaicedo <juan.caicedo1@alumnos.uneatlantico.es>
2025-05-16 11:33:55 +0200

    cambio nombrs de variables

--- Sucesionfibonacci_PY/Secesionfibonacci.py
+++ Sucesionfibonacci_PY/Secesionfibonacci.py
@@ -1,15 +1,15 @@
-def fibonacci(n):
+def fibonacci(userInput):
     sequence = []
 
-    if n <= 0:
+    if userInput <= 0:
         return sequence
-    elif n == 1:
+    elif userInput == 1:
         return [0]
-    elif n == 2:
+    elif userInput == 2:
         return [0, 1]
 
     sequence = [0, 1]
-    for i in range(2, n):
+    for i in range(2, userInput):
         next_value = sequence[i - 1] + sequence[i - 2]
         sequence.append(next_value)
 
